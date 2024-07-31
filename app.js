074d8e562ddf3ac40c6f28b58a3835b91b87d729
AguTrachta <142672522+AguTrachta@users.noreply.github.com>
2024-07-31 12:16:11 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,30 +2,27 @@
 
 document.getElementById('audio-upload').addEventListener('change', function(e) {
     const file = e.target.files[0];
-    const formData = new FormData();
-    formData.append('audio', file);
+    const reader = new FileReader();
+    reader.onload = function(event) {
+        const audioContent = event.target.result.split(',')[1];
 
-    fetch('/transcribe', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('transcription-text').textContent = data.transcription;
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-});
-
-document.getElementById('start-recording').addEventListener('click', function() {
-    // Aquí se iniciará la grabación de audio
-    console.log('Iniciar grabación');
-});
-
-document.getElementById('stop-recording').addEventListener('click', function() {
-    // Aquí se detendrá la grabación de audio
-    console.log('Detener grabación');
+        fetch('https://us-central1-decent-glazing-431020-n5.cloudfunctions.net/transcribeAudio'
+            , {  // Reemplaza con la URL de la función de Firebase
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ audioContent })
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('transcription-text').textContent = data.transcription;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    };
+    reader.readAsDataURL(file);
 });
 
 document.getElementById('generate-pdf').addEventListener('click', function() {
